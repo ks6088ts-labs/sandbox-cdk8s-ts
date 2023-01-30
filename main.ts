fee093de03d0193c9701236f24c5d83e47de3107
ks6088ts <ks6088ts@gmail.com>
2023-01-30 21:54:16 +0900

    add cron job example

--- main.ts
+++ main.ts
@@ -2,9 +2,12 @@ import { App } from 'cdk8s'
 import { HelloChart } from './lib/hello-chart'
 import { HelloPlusChart } from './lib/hello-plus-chart'
 import { StaticSiteChart } from './lib/static-site-chart'
+import { CronJobChart } from './lib/cron-job-chart'
 
 const app = new App()
 new HelloChart(app, 'hello')
 new HelloPlusChart(app, 'hello-plus')
 new StaticSiteChart(app, 'static-site')
+new CronJobChart(app, 'cron-job')
+
 app.synth()
